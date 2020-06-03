afe66e6c040f21ce4dccfb07ec6db859af01348d
Rachel S. Lee <rslee0025@gmail.com>
2020-06-03 23:53:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
-end
+
+
+  def key_for_min_value(hash)  
+ hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:sort_by)
+
+    key_for_min_value(hash)
+  end
